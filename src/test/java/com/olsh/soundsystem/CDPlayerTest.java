8e32268ce5ec9205595df6f67a193fadba83dbc5
Olha Schekera <kaltregen@gmail.com>
2016-04-28 15:14:26 +0300

    Update CDPlayerTest.java

--- src/test/java/com/olsh/soundsystem/CDPlayerTest.java
+++ src/test/java/com/olsh/soundsystem/CDPlayerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by pc on 24.04.2016.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = SgtPeppers.class)
 public class CDPlayerTest {
     @Autowired
     private CompactDisk disk;
